d8d1836b5deef84e1005ebbe3f7093c36d3a706c
Aneesh Makala <amakala@yugabyte.com>
2022-12-21 17:19:40 +0530

    file sink module initial commit

---
+++ debezium-server/debezium-server-dist/cpfile.sh
@@ -0,0 +1,1 @@
+cp ~/Documents/debezium/debezium-server/debezium-server-file/target/debezium-server-file-2.1.0-SNAPSHOT.jar ~/Documents/debezium/debezium-server/debezium-server-dist/target/debezium-server/lib/
---
+++ debezium-server/debezium-server-file/src/main/java/io/debezium/server/file/FileChangeConsumer.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.server.file;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.debezium.engine.ChangeEvent;
+import io.debezium.engine.DebeziumEngine;
+import io.debezium.server.BaseChangeConsumer;
+
+/**
+ * Implementation of the consumer that delivers the messages to an HTTP Webhook destination.
+ *
+ * @author Chris Baumbauer
+ */
+@Named("file")
+@Dependent
+public class FileChangeConsumer extends BaseChangeConsumer implements DebeziumEngine.ChangeConsumer<ChangeEvent<Object, Object>> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileChangeConsumer.class);
+
+    private static final String PROP_PREFIX = "debezium.sink.filesink.";
+
+    @ConfigProperty(name = PROP_PREFIX + "dataDir")
+    String dataDir;
+
+    Map<String, BufferedWriter> writers = new HashMap<>();
+    JsonFactory factory = new JsonFactory();
+    ObjectMapper mapper = new ObjectMapper(factory);
+
+    @PostConstruct
+    void connect() throws URISyntaxException {
+        LOGGER.info("connect() called: dataDir = {}", dataDir);
+        Thread t = new Thread(this::flush);
+        t.setDaemon(true);
+        t.start();
+    }
+
+    void flush() {
+        LOGGER.info("XXX Started flush thread.");
+        while (true) {
+            for (var writer : writers.values()) {
+                try {
+                    writer.flush();
+                }
+                catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            try {
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e) {
+                // Noop.
+            }
+        }
+    }
+
+    @Override
+    public void handleBatch(List<ChangeEvent<Object, Object>> records, DebeziumEngine.RecordCommitter<ChangeEvent<Object, Object>> committer)
+            throws InterruptedException {
+        LOGGER.info("RECEIVED BATCH IN FILE SINK" + "Size of records-" + records.size());
+        for (ChangeEvent<Object, Object> record : records) {
+            // Object objKey = record.key();
+            Object objVal = record.value();
+
+            var r = parse((String) objVal);
+            if (r == null) {
+                LOGGER.info("XXX Skipped: {}", objVal);
+                continue;
+            }
+            LOGGER.info("{}.{}.{} => {}", r.dbName, r.schemaName, r.tableName, r.rowText);
+            try {
+                writeRecord(r);
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            // committer.markProcessed(record);
+        }
+        committer.markBatchFinished();
+    }
+
+    private void writeRecord(Record r) throws IOException {
+        var fileName = dataDir + "/" + r.dbName + "-" + r.schemaName + "-" + r.tableName;
+        var writer = writers.get(fileName);
+        if (writer == null) {
+            var f = new FileWriter(fileName);
+            writer = new BufferedWriter(f);
+            writers.put(fileName, writer);
+        }
+        writer.write(r.rowText);
+        writer.write('\n');
+        // writer.flush();
+    }
+
+    public Record parse(String json) {
+        try {
+            JsonNode rootNode = mapper.readTree(json);
+            LOGGER.info("XXX Received JSON {}", json);
+
+            var payload = rootNode.get("payload");
+            LOGGER.info("XXX Received Payload {}", payload.toPrettyString());
+
+            if (payload == null || payload.isNull()) {
+                LOGGER.info("XXX payload is null {}", json);
+                return null;
+            }
+            var op = payload.get("op");
+            if (op == null || op.isNull()) {
+                LOGGER.info("XXX op is null {}", json);
+                return null;
+            }
+            if (!op.asText().equals("r")) {
+                LOGGER.info("XXX op not r {}", json);
+                return null;
+            }
+            var after = payload.get("after");
+            if (after == null || after.isNull()) {
+                LOGGER.info("XXX after is null {}", json);
+                return null;
+            }
+
+            var fields = after.fields();
+            var values = new ArrayList<String>();
+            while (fields.hasNext()) {
+                var f = fields.next();
+                var v = f.getValue().toString();
+                values.add(v);
+            }
+            var text = String.join("\t", values);
+
+            var source = payload.get("source");
+            var r = new Record();
+            r.dbName = source.get("db").asText();
+            r.schemaName = source.get("schema").asText();
+            r.tableName = source.get("table").asText();
+            r.rowText = text;
+
+            return r;
+        }
+        catch (Exception ex) {
+            LOGGER.error("XXX parse: {}", ex);
+        }
+        LOGGER.info("XXX end returning NULL {}", json);
+        return null;
+    }
+}
+
+class Record {
+    String dbName, schemaName, tableName;
+    String rowText;
+}
