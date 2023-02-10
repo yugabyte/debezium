5165a8c65f7a8f113f5f7822585e7bdbbdbace76
Aneesh Makala <amakala@yugabyte.com>
2023-02-10 12:21:46 +0530

    test with postgres_text

--- debezium-server/debezium-server-ybexporter/src/main/java/io/debezium/server/ybexporter/TableSnapshotWriterCSV.java
+++ debezium-server/debezium-server-ybexporter/src/main/java/io/debezium/server/ybexporter/TableSnapshotWriterCSV.java
@@ -37,9 +37,9 @@ public class TableSnapshotWriterCSV implements RecordWriter {
             fd = fos.getFD();
             var f = new FileWriter(fd);
             var bufferedWriter = new BufferedWriter(f);
-            csvPrinter = new CSVPrinter(bufferedWriter, CSVFormat.POSTGRESQL_CSV);
+            csvPrinter = new CSVPrinter(bufferedWriter, CSVFormat.POSTGRESQL_TEXT);
             ArrayList<String> cols = t.getColumns();
-            String header = String.join(CSVFormat.POSTGRESQL_CSV.getDelimiterString(), cols) + CSVFormat.POSTGRESQL_CSV.getRecordSeparator();
+            String header = String.join(CSVFormat.POSTGRESQL_TEXT.getDelimiterString(), cols) + CSVFormat.POSTGRESQL_TEXT.getRecordSeparator();
             LOGGER.debug("header = {}", header);
             f.write(header);
             es = ExportStatus.getInstance(dataDir);
