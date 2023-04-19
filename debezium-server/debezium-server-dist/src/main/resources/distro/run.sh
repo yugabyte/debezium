5a0b47b612131d6dcc47ee207dfeb3bfaffdfb41
Aneesh Makala <amakala@yugabyte.com>
2023-04-19 13:27:08 +0530

    alternate solution: absolute path instead of relative paths in run.sh

--- debezium-server/debezium-server-dist/src/main/resources/distro/run.sh
+++ debezium-server/debezium-server-dist/src/main/resources/distro/run.sh
@@ -5,6 +5,8 @@
 # Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
 #
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 if [ -z "$JAVA_HOME" ]; then
   JAVA_BINARY="java"
 else
@@ -17,18 +19,13 @@ else
   PATH_SEP=":"
 fi
 
-if [ -z "$DEBEZIUM_DIST_DIR" ]; then
-    DEBEZIUM_DIST_DIR="./"
-fi
-
-RUNNER=$(ls "$DEBEZIUM_DIST_DIR"/debezium-server-*runner.jar)
+RUNNER=$(ls "$SCRIPT_DIR"/debezium-server-*runner.jar)
 
 ENABLE_DEBEZIUM_SCRIPTING=${ENABLE_DEBEZIUM_SCRIPTING:-false}
-LIB_PATH="$DEBEZIUM_DIST_DIR/lib/*"
+LIB_PATH="$SCRIPT_DIR/lib/*"
 if [[ "${ENABLE_DEBEZIUM_SCRIPTING}" == "true" ]]; then
-    LIB_PATH=$LIB_PATH$PATH_SEP"$DEBEZIUM_DIST_DIR/lib_opt/*"
+    LIB_PATH=$LIB_PATH$PATH_SEP"$SCRIPT_DIR/lib_opt/*"
 fi
-
 
 DEBUGGER="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
 exec "$JAVA_BINARY" $DEBEZIUM_OPTS $JAVA_OPTS -cp "$RUNNER"$PATH_SEP"conf"$PATH_SEP$LIB_PATH "$DEBUGGER" io.debezium.server.Main
