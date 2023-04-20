9e9ee52c074dacc9139c5a01a7cfea61d0a03241
Aneesh Makala <amakala@yugabyte.com>
2023-04-20 12:46:05 +0530

    pass properties file to run.sh

--- debezium-server/debezium-server-dist/src/main/resources/distro/run.sh
+++ debezium-server/debezium-server-dist/src/main/resources/distro/run.sh
@@ -7,6 +7,16 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+if [ -z "$1" ]; then
+    echo "Argument missing. Provide application.properties file path as argument";
+    exit 1;
+fi
+PROPERTIES_FILE_PATH="$1"
+if [ ! -f "$PROPERTIES_FILE_PATH" ]; then
+    echo "$PROPERTIES_FILE_PATH does not exist."
+    exit 1;
+fi
+
 if [ -z "$JAVA_HOME" ]; then
   JAVA_BINARY="java"
 else
@@ -27,5 +37,6 @@ if [[ "${ENABLE_DEBEZIUM_SCRIPTING}" == "true" ]]; then
     LIB_PATH=$LIB_PATH$PATH_SEP"$SCRIPT_DIR/lib_opt/*"
 fi
 
+
 DEBUGGER="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
-exec "$JAVA_BINARY" $DEBEZIUM_OPTS $JAVA_OPTS -cp "$RUNNER"$PATH_SEP"conf"$PATH_SEP$LIB_PATH "$DEBUGGER" io.debezium.server.Main
+exec "$JAVA_BINARY" $DEBEZIUM_OPTS $JAVA_OPTS -cp "$RUNNER"$PATH_SEP"conf"$PATH_SEP$LIB_PATH -Dquarkus.config.locations=$PROPERTIES_FILE_PATH "$DEBUGGER" io.debezium.server.Main
