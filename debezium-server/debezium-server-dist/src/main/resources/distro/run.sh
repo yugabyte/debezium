07ac5cf1ebde6deaba418000862b5ccc3f0a04e2
Aneesh Makala <amakala@yugabyte.com>
2023-05-22 16:01:29 +0530

    Java check in run.sh script (#56)
    * java check to run.sh

--- debezium-server/debezium-server-dist/src/main/resources/distro/run.sh
+++ debezium-server/debezium-server-dist/src/main/resources/distro/run.sh
@@ -7,6 +7,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+
+# Read properties file argument
 if [ -z "$1" ]; then
     echo "Argument missing. Provide application.properties file path as argument";
     exit 1;
@@ -17,12 +19,25 @@ if [ ! -f "$PROPERTIES_FILE_PATH" ]; then
     exit 1;
 fi
 
+
+# Resolve Java
 if [ -z "$JAVA_HOME" ]; then
   JAVA_BINARY="java"
 else
   JAVA_BINARY="$JAVA_HOME/bin/java"
 fi
+MIN_REQUIRED_MAJOR_VERSION='17'
+JAVA_MAJOR_VER=$(${JAVA_BINARY} -version 2>&1 | awk -F '"' '/version/ {print $2}' | awk -F. '{print $1}')
+if ([ -n "$JAVA_MAJOR_VER" ] && (( 10#${JAVA_MAJOR_VER} >= 10#${MIN_REQUIRED_MAJOR_VERSION} )) ) #integer compare of versions.
+then
+    echo "Found sufficient java version = ${JAVA_MAJOR_VER}"
+else
+    echo "ERROR: Java not found or insufficient version ${JAVA_MAJOR_VER}. Please install java>=${MIN_REQUIRED_MAJOR_VERSION}"
+    exit 1;
+fi
 
+
+# Run
 if [ "$OSTYPE" = "msys" ] || [ "$OSTYPE" = "cygwin" ]; then
   PATH_SEP=";"
 else
