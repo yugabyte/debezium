eaa65cb1881ff8c62159961fd09812076fe32b41
Aneesh Makala <amakala@yugabyte.com>
2023-06-15 13:51:20 +0530

    building prereq components in script to potentially invalidate caches

--- buildDebeziumServer.sh
+++ buildDebeziumServer.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+# prereqs
 make debezium
+# rebuiling because we make changes to these components, and it may be cached. TODO: find better alternative for this.
+make debezium-core
+make debezium-server-core
+make postgres-connector
+
 make debezium-server
