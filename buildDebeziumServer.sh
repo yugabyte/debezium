1911a09165d9b6a9625b716f8de4ba17c81b0579
Aneesh Makala <amakala@yugabyte.com>
2023-06-15 14:27:50 +0530

    reorder make commands

--- buildDebeziumServer.sh
+++ buildDebeziumServer.sh
@@ -3,9 +3,9 @@ set -e
 
 # prereqs
 make debezium
+make debezium-server
+
 # rebuiling because we make changes to these components, and it may be cached. TODO: find better alternative for this.
 make debezium-core
 make debezium-server-core
 make postgres-connector
-
-make debezium-server
