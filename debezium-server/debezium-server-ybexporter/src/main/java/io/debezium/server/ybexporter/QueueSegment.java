9194c0ee7708ed4a6761d49e02e33115fbeb6c32
Aneesh Makala <amakala@yugabyte.com>
2023-07-10 13:36:12 +0530

    class for QueueSegment

---
+++ debezium-server/debezium-server-ybexporter/src/main/java/io/debezium/server/ybexporter/QueueSegment.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.server.ybexporter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QueueSegment {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueueSegment.class);
+
+    public QueueSegment(String filePath){
+
+    }
+    public void write(Record r){
+
+    }
+    public void flush(){
+
+    }
+    public void close(){
+
+    }
+    public void sync(){
+
+    }
+}
