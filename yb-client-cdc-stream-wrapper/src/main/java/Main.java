c656b4c6de5f1f47f85c97b3f890a5b45ce55151
pgupta@yugabtye.com <centos@ip-10-9-14-120.us-west-2.compute.internal>
2023-08-10 10:04:52 +0000

    modifications

--- yb-client-cdc-stream-wrapper/src/main/java/Main.java
+++ yb-client-cdc-stream-wrapper/src/main/java/Main.java
@@ -17,27 +17,29 @@ public class Main {
                 .build();
 
         YBClient client = new YBClient(asyncClient);
-        YBTable table = null;
-        try {
-            ListTablesResponse resp = client.getTablesList();
-            for (TableInfo tableInfo : resp.getTableInfoList()) {
-                if (Objects.equals(tableInfo.getName(), parameters.tableName)) {
-                    table =  client.openTableByUUID(tableInfo.getId().toStringUtf8());
-                }
-            }
-        } catch (Exception ex) {
-            throw ex;
-        }
-        if (table == null) {
-            throw new RuntimeException("No table found with the specified name");
-        }
-        if (parameters.toCreate) {
-            String streamID = client.createCDCStream(table, parameters.dbName, "PROTO", "IMPLICIT", "ALL").getStreamId();
-            System.out.println("CDC Stream ID: " + streamID);
-        } else if (parameters.deleteStreamId != null) {
+
+        if (parameters.deleteStreamId != null) {
             Set<String> streamIds = new HashSet<String>();
             streamIds.add(parameters.deleteStreamId);
             client.deleteCDCStream(streamIds, false, true);
+        }  else if (parameters.toCreate) {
+            YBTable table = null;
+            try {
+                ListTablesResponse resp = client.getTablesList();
+                for (TableInfo tableInfo : resp.getTableInfoList()) {
+                    String qualifiedTableName = String.format("%s.%s", tableInfo.getPgschemaName(), tableInfo.getName());
+                    if (Objects.equals(qualifiedTableName, parameters.tableName)) {
+                        table =  client.openTableByUUID(tableInfo.getId().toStringUtf8());
+                    }
+                }
+            } catch (Exception ex) {
+                throw ex;
+            }
+            if (table == null) {
+                throw new RuntimeException("No table found with the specified name");
+            }
+            String streamID = client.createCDCStream(table, parameters.dbName, "PROTO", "IMPLICIT", "ALL").getStreamId();
+            System.out.println("CDC Stream ID: " + streamID);
         } else {
             throw new RuntimeException("Either create or delete stream id should be specified");
         }
