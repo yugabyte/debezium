a6b64182785269ce7a5ebd8698e0b66997cd7a5c
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2024-03-26 13:09:05 +0530

    cleanup of the transformer

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/transforms/yugabytedb/YBExtractNewRecordState.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/transforms/yugabytedb/YBExtractNewRecordState.java
@@ -106,10 +106,6 @@ public class YBExtractNewRecordState<R extends ConnectRecord<R>> extends Extract
 
 class SchemaUtil {
 
-  public static SchemaBuilder copySchemaBasics(Schema source) {
-    return copySchemaBasics(source, new SchemaBuilder(source.type()));
-  }
-
   public static SchemaBuilder copySchemaBasics(Schema source, SchemaBuilder builder) {
     builder.name(source.name());
     builder.version(source.version());
