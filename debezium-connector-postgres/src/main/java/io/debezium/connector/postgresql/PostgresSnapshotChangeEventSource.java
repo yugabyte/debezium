671e0227b0d7a4feb77bd73294ab957d919b7dd2
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2024-06-04 16:11:49 +0530

    added config to skip consistent snapshot

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresSnapshotChangeEventSource.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresSnapshotChangeEventSource.java
@@ -269,10 +269,14 @@ public class PostgresSnapshotChangeEventSource extends RelationalSnapshotChangeE
     }
 
     protected void setSnapshotTransactionIsolationLevel(boolean isOnDemand) throws SQLException {
-        LOGGER.info("Setting isolation level");
-        String transactionStatement = snapshotter.snapshotTransactionIsolationLevelStatement(slotCreatedInfo, isOnDemand);
-        LOGGER.info("Opening transaction with statement {}", transactionStatement);
-        jdbcConnection.executeWithoutCommitting(transactionStatement);
+        if (connectorConfig.isYbConsistentSnapshotEnabled()) {
+            LOGGER.info("Setting isolation level");
+            String transactionStatement = snapshotter.snapshotTransactionIsolationLevelStatement(slotCreatedInfo, isOnDemand);
+            LOGGER.info("Opening transaction with statement {}", transactionStatement);
+            jdbcConnection.executeWithoutCommitting(transactionStatement);
+        } else {
+            LOGGER.info("Skipping setting snapshot time, snapshot data will not be consistent");
+        }
     }
 
     /**
