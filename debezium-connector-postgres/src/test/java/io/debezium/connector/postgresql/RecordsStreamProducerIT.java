150255daa840b99a52ed845254ce36fcbd4b7ecd
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2024-07-11 17:40:21 +0530

    added more tests to be run

--- debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/RecordsStreamProducerIT.java
+++ debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/RecordsStreamProducerIT.java
@@ -23,7 +23,6 @@ import static org.junit.Assert.assertNotNull;
 
 import java.math.BigDecimal;
 import java.nio.ByteBuffer;
-import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
