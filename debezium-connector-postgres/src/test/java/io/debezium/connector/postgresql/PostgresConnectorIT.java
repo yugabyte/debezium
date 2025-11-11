cbca104b9ee2a8c26a3e08beb14d73d2a6bfa9f8
Conor Gallagher <conor.gallagher@zalando.ie>
2025-11-11 19:25:17 +0000

    DBZ-9641 - remove comment
    (cherry picked from commit 7a2a7a8bccc2798c8b8ec66044067a1aa1a9adcf)

--- debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/PostgresConnectorIT.java
+++ debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/PostgresConnectorIT.java
@@ -3282,7 +3282,6 @@ public class PostgresConnectorIT extends AbstractConnectorTest {
 
     @Test
     @FixFor("DBZ-5811")
-    // Tests deprecated SHOULD_FLUSH_LSN_IN_SOURCE_DB property for backward compatibility
     public void shouldNotAckLsnOnSource() throws Exception {
         TestHelper.dropDefaultReplicationSlot();
         TestHelper.execute(SETUP_TABLES_STMT);
