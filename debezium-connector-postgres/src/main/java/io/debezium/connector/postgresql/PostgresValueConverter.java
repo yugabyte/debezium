cd1037200da1f71a9158c14e86ca3e365d0675ce
Shishir Sharma <ssharma@yugabyte.com>
2025-12-15 05:07:13 +0000

    Revert Java 11 changes

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresValueConverter.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresValueConverter.java
@@ -668,46 +668,43 @@ public class PostgresValueConverter extends JdbcValueConverters {
     }
 
     private Object convertPgVector(Column column, Field fieldDefn, Object data) {
-        // YB: Changed from Java 16+ pattern matching instanceof to Java 11 compatible explicit casts
         return convertValue(column, fieldDefn, data, Collections.emptyList(), r -> {
-            if (data instanceof byte[]) {
-                r.deliver(Vector.fromLogical(fieldDefn.schema(), new String((byte[]) data, databaseCharset)));
+            if (data instanceof byte[] typedData) {
+                r.deliver(Vector.fromLogical(fieldDefn.schema(), new String(typedData, databaseCharset)));
             }
-            else if (data instanceof String) {
-                r.deliver(Vector.fromLogical(fieldDefn.schema(), (String) data));
+            if (data instanceof String typedData) {
+                r.deliver(Vector.fromLogical(fieldDefn.schema(), typedData));
             }
-            else if (data instanceof PGobject) {
-                r.deliver(Vector.fromLogical(fieldDefn.schema(), ((PGobject) data).getValue()));
+            else if (data instanceof PGobject typedData) {
+                r.deliver(Vector.fromLogical(fieldDefn.schema(), typedData.getValue()));
             }
         });
     }
 
     private Object convertPgHalfVector(Column column, Field fieldDefn, Object data) {
-        // YB: Changed from Java 16+ pattern matching instanceof to Java 11 compatible explicit casts
         return convertValue(column, fieldDefn, data, Collections.emptyList(), r -> {
-            if (data instanceof byte[]) {
-                r.deliver(HalfVector.fromLogical(fieldDefn.schema(), new String((byte[]) data, databaseCharset)));
+            if (data instanceof byte[] typedData) {
+                r.deliver(HalfVector.fromLogical(fieldDefn.schema(), new String(typedData, databaseCharset)));
             }
-            else if (data instanceof String) {
-                r.deliver(HalfVector.fromLogical(fieldDefn.schema(), (String) data));
+            if (data instanceof String typedData) {
+                r.deliver(HalfVector.fromLogical(fieldDefn.schema(), typedData));
             }
-            else if (data instanceof PGobject) {
-                r.deliver(HalfVector.fromLogical(fieldDefn.schema(), ((PGobject) data).getValue()));
+            else if (data instanceof PGobject typedData) {
+                r.deliver(HalfVector.fromLogical(fieldDefn.schema(), typedData.getValue()));
             }
         });
     }
 
     private Object convertPgSparseVector(Column column, Field fieldDefn, Object data) {
-        // YB: Changed from Java 16+ pattern matching instanceof to Java 11 compatible explicit casts
         return convertValue(column, fieldDefn, data, Collections.emptyList(), r -> {
-            if (data instanceof byte[]) {
-                r.deliver(SparseVector.fromLogical(fieldDefn.schema(), new String((byte[]) data, databaseCharset)));
+            if (data instanceof byte[] typedData) {
+                r.deliver(SparseVector.fromLogical(fieldDefn.schema(), new String(typedData, databaseCharset)));
             }
-            else if (data instanceof String) {
-                r.deliver(SparseVector.fromLogical(fieldDefn.schema(), (String) data));
+            if (data instanceof String typedData) {
+                r.deliver(SparseVector.fromLogical(fieldDefn.schema(), typedData));
             }
-            else if (data instanceof PGobject) {
-                r.deliver(SparseVector.fromLogical(fieldDefn.schema(), ((PGobject) data).getValue()));
+            else if (data instanceof PGobject typedData) {
+                r.deliver(SparseVector.fromLogical(fieldDefn.schema(), typedData.getValue()));
             }
         });
     }
